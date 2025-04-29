b97c39f38b7595cfc92216636d72ce556be74818
matthewKeville <matthewkeville@protonmail.com>
2025-04-29 16:54:16 -0400

    simplified schema for methods, removing single method inspection, added new requestType info, updated compilation tests

--- tests/schemas-compilation-test.js
+++ tests/schemas-compilation-test.js
@@ -1,5 +1,6 @@
 import Ajv from "ajv";
 import fs from "node:fs";
+import jest from 'jest';
 
 const ajv = new Ajv();
 
@@ -8,27 +9,35 @@ const schemas = [
   ["schemas/error-schema.json"],
   ["schemas/puzzle-data-schema.json"],
 
+  ["schemas/info/info-response-body-schema.json"],
+
   ["schemas/fetch/fetch-request-body-schema.json"],
   ["schemas/fetch/fetch-response-body-schema.json"],
 
   ["schemas/methods/method-schema.json"],
   ["schemas/methods/methods-response-body-schema.json"],
-  ["schemas/methods/methods-request-body-schema.json"],
 
   ["schemas/request-schema.json"],
   ["schemas/response-schema.json"],
 ]
 
+  schemas.forEach( sch => {
+    test(`${sch} parses`, () => {
+      var schemaJson = JSON.parse(fs.readFileSync(sch[0], 'utf8'));
+      sch.push(schemaJson)
+    })
+  })
+
+
 schemas.forEach( sch => {
-  var schemaJson = JSON.parse(fs.readFileSync(sch[0], 'utf8'));
-  sch.push(schemaJson)
+  test(`${sch} added`, () => {
+    ajv.addSchema(sch[1],sch[1].$Id)
+  })
 })
 
 schemas.forEach( sch => {
-  ajv.addSchema(sch[1],sch[1].$Id)
-})
-
-schemas.forEach( sch => {
-  ajv.compile(sch[1])
+  test(`${sch} compiles`, () => {
+    ajv.compile(sch[1])
+  })
 })
 
